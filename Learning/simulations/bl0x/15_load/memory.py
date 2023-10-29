5588ec11570cefcaad22276be572149d532c8f32
wdevore <william.quartz@gmail.com>
2023-10-29 14:47:53 -0400

    added 14 and 15 bl0x steps. 15 was missing rs1,rs2 public exports

--- Learning/simulations/bl0x/15_load/memory.py
+++ Learning/simulations/bl0x/15_load/memory.py
@@ -14,22 +14,24 @@ class Memory(Elaboratable):
         a = RiscvAssembler()
 
         a.read("""begin:
-        ADD x10, x0, x0
+        LI  s0, 0
+        LI  s1, 16
 
         l0:
-        ADDI x10, x10, 1
-        JAL x1, wait
-        JAL zero, l0
+        LB   a0, s0, 400
+        CALL wait
+        ADDI s0, s0, 1
+        BNE  s0, s1, l0
         EBREAK
 
         wait:
-        ADDI x11, x0, 1
-        SLLI x11, x11, 20
+        LI   t0, 1
+        SLLI t0, t0, 20
 
         l1:
-        ADDI x11, x11, -1
-        BNE x11, x0, l1
-        JALR x0, x1, 0
+        ADDI t0, t0, -1
+        BNEZ t0, l1
+        RET
         """)
 
         a.assemble()
@@ -37,14 +39,24 @@ class Memory(Elaboratable):
         print("memory = {}".format(self.instructions))
 
         # Instruction memory initialised with above instructions
-        self.mem = Array([Signal(32, reset=x, name="mem")
-                          for x in self.instructions])
+        self.mem = Array([Signal(32, reset=x, name="mem{}".format(i))
+                          for i,x in enumerate(self.instructions)])
 
         self.mem_addr = Signal(32)
         self.mem_rdata = Signal(32)
         self.mem_rstrb = Signal()
 
-    def elaborate(self, platform: Platform) -> Module:
+        while(len(self.mem) < 100):
+            self.mem.append(0)
+        
+        self.mem.append(0x04030201)
+        self.mem.append(0x08070605)
+        self.mem.append(0x0c0b0a09)
+        self.mem.append(0xff0f0e0d)
+
+        print(self.mem)
+
+    def elaborate(self, platform):
         m = Module()
 
         with m.If(self.mem_rstrb):
